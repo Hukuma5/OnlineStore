4c456211a9547120816d0b1b10a595ee1d163349
Hukuma5 <56114535+Hukuma5@users.noreply.github.com>
2022-04-26 14:11:39 +0300

    Add files via upload

---
+++ catalog1_202204260842.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.catalog1 (article,"name",price) VALUES
+	 ('123456789012','Notebook Asus',60000.0),
+	 ('565763893500','MacBook Pro',160000.0),
+	 ('657467486933','MacBook Air',120000.0),
+	 ('347583649850','Iphone ProMax',120000.0),
+	 ('586749395254','Iphone Pro',100000.0),
+	 ('098764736541','Iphone',89000.0);
